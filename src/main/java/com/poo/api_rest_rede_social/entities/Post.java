9dbb25698e155593b570cf2412ce8d37a1cc79db
Henrique Azevedo <henriquee.aze.dev@gmail.com>
2024-11-03 09:29:16 -0300

    Mudando schemas do MySQL

--- src/main/java/com/poo/api_rest_rede_social/entities/Post.java
+++ src/main/java/com/poo/api_rest_rede_social/entities/Post.java
@@ -2,41 +2,34 @@ package com.poo.api_rest_rede_social.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
-@Data
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Table(name = "users", schema = "apiredesocial")
-public class User {
+@Table(name = "posts", schema = "apiredesocial")
+public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-
-    private String email;
-
-    @Column(unique = true)
-    private String username;
-
-    private String bio;
-
-    private String password;
-
-    private Boolean active;
+    @Column(name = "text_content")
+    private String textContent;
 
     @CreationTimestamp
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    @ManyToOne
+    @JoinColumn(name = "users_id")
+    private User user;
 }
